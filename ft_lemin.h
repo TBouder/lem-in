0976eabaaaf1c9f22e5ca02ea8448be4bb349a31
Tbouder <tbouder@protonmail.com>
2016-04-22 17:19:57 +0200

    Stat parsing + unit test to detect if file is valid or not

--- ft_lemin.h
+++ ft_lemin.h
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/22 12:14:46 by tbouder           #+#    #+#             */
-/*   Updated: 2016/04/22 12:18:51 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/04/22 16:00:57 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,37 @@
 
 # include "ft_printf/ft_printf.h"
 # include <fcntl.h>
+
+typedef struct		s_pipes
+{
+	long			ant;
+	int				occuped;
+	struct s_pipes	*prev;
+	struct s_pipes	*next;
+}					t_pipes;
+
+typedef struct		s_rooms
+{
+	char			*name;
+	int				x;
+	int				y;
+	struct s_pipes	*rooms;
+	struct s_rooms	*prev;
+	struct s_rooms	*next;
+}					t_rooms;
+
+typedef struct		s_env
+{
+	long			ant;
+	struct s_rooms	*rooms;
+}					t_env;
+
+void	ft_error(char *str);
+long	ft_atoi_onum(const char *str);
+
+void	ft_open_stdin(t_env *env);
+void	ft_open_one(t_env *env, char **av);
+void	ft_open_mult(t_env *env, char *map);
+
 
 #endif
---
+++ ft_tools.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tools.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/22 15:51:08 by tbouder           #+#    #+#             */
+/*   Updated: 2016/04/22 16:35:02 by tbouder          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_lemin.h"
+
+void	ft_error(char *str)
+{
+	ft_putendl_fd(str, 2);
+	exit(EXIT_FAILURE);
+}
+
+long	ft_atoi_onum(const char *str)
+{
+	long	result;
+	int		sign;
+
+	sign = 1;
+	result = 0;
+	if (ft_strlen(str) == 0 || (*str < 32 && !ft_isspace(*str)))
+		ft_error("Ant error : number of ant cannot be 0");
+	while (*str <= 32)
+		str++;
+	if ((*str == '-' || *str == '+'))
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (ft_isnumber(*str))
+		result = (result + *str++ - '0') * 10;
+	if (!(ft_isnumber(*str)) && *str != '\0')
+		ft_error("Ant error : number of ant must be a number");
+	if (result / 10 * sign == 0)
+		ft_error("Ant error : number of ant cannot be 0");
+	return (result / 10 * sign);
+}
