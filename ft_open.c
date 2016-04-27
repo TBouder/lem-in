78bef883f53705bd06ab1df4001c64d0ad499ac7
Tbouder <tbouder@protonmail.com>
2016-04-27 15:16:33 +0200

    Leaks ok

--- ft_open.c
+++ ft_open.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/22 15:25:58 by tbouder           #+#    #+#             */
-/*   Updated: 2016/04/27 12:54:58 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/04/27 15:16:13 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,18 @@ void		ft_open_stdin(t_env *env)
 	{
 		str = ft_strnew(ft_strlen(env->buff));
 		env->map = ft_strjoin_endl(str, env->buff);
+		if (!ft_isdigit(env->buff[0]))
+			ft_error(env, "Ant {r}error{0} : number of ant must be a number > to 0");
 		env->ant = ft_atoi_onum(env->buff);
 		ft_strdel(&str);
 		if (env->ant == -1)
-		{
-			ft_free_all(&env, 0);
 			ft_error(env, "Ant {r}error{0} : number of ant must be a number > to 0");
-		}
 	}
 	else
-		ft_error(env, "File {r}error{0} : arg must be a file");
+	{
+		ft_printf("File {r}error{0} : arg must be a file\n");
+		exit(EXIT_FAILURE);
+	}
 	ft_extract_map(env, NULL, NULL);
 }
 
@@ -44,15 +46,17 @@ void		ft_open_file(t_env *env, char *map)
 	{
 		str = ft_strnew(ft_strlen(env->buff));
 		env->map = ft_strjoin_endl(str, env->buff);
+		if (!ft_isdigit(env->buff[0]))
+			ft_error(env, "Ant {r}error{0} : number of ant must be a number > to 0");
 		env->ant = ft_atoi_onum(env->buff);
 		ft_strdel(&str);
 		if (env->ant == -1)
-		{
-			ft_free_all(&env, 0);
 			ft_error(env, "Ant {r}error{0} : number of ant must be a number > to 0");
-		}
 	}
 	else
-		ft_error(env, "File {r}error{0} : arg must be a file");
+	{
+		ft_printf("File {r}error{0} : arg must be a file\n");
+		exit(EXIT_FAILURE);
+	}
 	ft_extract_map(env, NULL, NULL);
 }
