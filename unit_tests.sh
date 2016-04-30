57ac9ebc7775af0786b3d4f59130fce20a944296
tbouder <tbouder@student.42.fr>
2016-04-30 13:24:33 +0200

    Update unit_test

--- unit_tests.sh
+++ unit_tests.sh
@@ -8,192 +8,165 @@ echo '/_____/\___/_/ /_/ /_/     /_/_/ /_/   \__/\___/____/\__/  '
 echo '                                     Tbouder@student.42.fr '
 echo
 
-red=$(tput setaf 1)
+red=$(tput bold ; tput setaf 1)
 green=$(tput setaf 2)
 yellow=$(tput bold ; tput setaf 3)
 blue=$(tput bold ; tput setaf 6)
 normal=$(tput sgr0)
 
+function signalerr
+{
+	if [[ $1 -ge 129 && $1 -le 140 ]]; then
+		errors[$i]=$(basename $4)
+		motif[$i]="SEGFAULT"
+		i=$((i+1))
+		printf "%s" "$red[SEGFAULT]$normal"
+	elif [[ $1 -eq 42 ]]; then
+		errors[$i]=$(basename $4)
+		motif[$i]="LEAKS"
+		i=$((i+1))
+		printf "%s" "$red[LEAKS]$normal"
+	elif [[ $2 == *"error"* || $2 == *"ERROR"* || $2 == *"Error"* ]]; then
+		printf "%s" "$green.$normal"
+	else
+		errors[$i]=$(basename $4)
+		motif[$i]="KO"
+		i=$((i+1))
+		printf "%s" "$red[KO]$normal"
+	fi
+}
+
 function signal
 {
-	signres=1
-	if [[ $1 -eq 129 ]]; then
-		printf "%34s\n" "$red[SIGHUP]$normal"
-	elif [[ $1 -eq 130 ]]; then
-		printf "%34s\n" "$red[SIGINT]$normal"
-	elif [[ $1 -eq 131 ]]; then
-		printf "%35s\n" "$red[SIGQUIT]$normal"
-	elif [[ $1 -eq 132 ]]; then
-		printf "%34s\n" "$red[SIGILL]$normal"
-	elif [[ $1 -eq 133 ]]; then
-		printf "%35s\n" "$red[SIGTRAP]$normal"
-	elif [[ $1 -eq 134 ]]; then
-		printf "%36s\n" "$red[SIGABORT]$normal"
-	elif [[ $1 -eq 135 ]]; then
-		printf "%34s\n" "$red[SIGEMT]$normal"
-	elif [[ $1 -eq 136 ]]; then
-		printf "%34s\n" "$red[SIGFPE]$normal"
-	elif [[ $1 -eq 137 ]]; then
-		printf "%35s\n" "$red[SIGKILL]$normal"
-	elif [[ $1 -eq 138 ]]; then
-		printf "%36s\n" "$red[BUSERROR]$normal"
-	elif [[ $1 -eq 139 ]]; then
-		printf "%36s\n" "$red[SEGFAULT]$normal"
-	elif [[ $1 -eq 140 ]]; then
-		printf "%36s\n" "$red[SIGSYS]$normal"
+	if [[ $1 -ge 129 && $1 -le 140 ]]; then
+		errors[$i]=$(basename $4)
+		motif[$i]="SEGFAULT"
+		i=$((i+1))
+		printf "%s" "$red[SEGFAULT]$normal"
+	elif [[ $1 -eq 42 ]]; then
+		errors[$i]=$(basename $4)
+		motif[$i]="LEAKS"
+		i=$((i+1))
+		printf "%s" "$red[LEAKS]$normal"
+	elif [[ "$2" == "" ]]; then
+		printf "%s" "$green.$normal"
 	else
-		signres=0
+		errors[$i]=$(basename $4)
+		motif[$i]="KO"
+		i=$((i+1))
+		printf "%s" "$red[KO]$normal"
 	fi
 }
 
 # Extract args
 # ---------------------------------------------------------------------------- #
-WAY=/Volumes/USB/lem-in/lem-in_maps
-i=0
-all=0
-autor=0
-makefile=0
-norme=0
-all=0
-leaks=""
-while [ $# -ne 0 ];do
-	if [ "$1" = "leaks" ]; then
-		leaks="sh /Volumes/USB/.files/valgrind/vg-in-place -q --leak-check=full --suppressions=/Volumes/USB/.files/valgrind/osx.supp --error-exitcode=42"
-	elif [ $1 = "autor" ]; then
-		autor=1
-	elif [ $1 = "makefile" ]; then
-		makefile=1
-	elif [ $1 = "norme" ]; then
-		norme=1
-	elif [ $1 = "all" ]; then
-		all=1
-	fi
-	shift
-done
-if [[ autor -eq 1 || all -eq 1 ]]; then
-	if [ -e "auteur" ]; then
-		user=$(cat auteur)
-		iam=$(whoami)
-		if [ "$user" = "$iam" ] ; then
-			echo "Author file : \033[32;1mOK\033[00;0m"
-		else
-			echo "Author file : \033[31;1mKO\033[00;0m"
+	WAY=/Volumes/USB/lem-in/lem-in_maps
+	i=0
+	j=0
+	all=0
+	autor=0
+	makefile=0
+	norme=0
+	all=0
+	leaks=""
+	while [ $# -ne 0 ];do
+		if [ "$1" = "leaks" ]; then
+			leaks="sh /Volumes/USB/.files/valgrind/vg-in-place -q --leak-check=full --suppressions=/Volumes/USB/.files/valgrind/osx.supp --error-exitcode=42"
+		elif [ $1 = "autor" ]; then
+			autor=1
+		elif [ $1 = "makefile" ]; then
+			makefile=1
+		elif [ $1 = "norme" ]; then
+			norme=1
+		elif [ $1 = "all" ]; then
+			all=1
 		fi
-	else
-		echo "\033[31;1mAuthor file missing\033[00;0m"
-	fi
-	echo
-fi
-if [[ makefile -eq 1 || all -eq 1 ]]; then
-	if [ -e "Makefile" ]; then
-		make fclean
-		make
-		make2=$(make)
-		make clean
-		# make3=$(make)
-		# make re
-		make
-		make fclean
-		# if [ "$make2" = "make: Nothing to be done for \`all'." -a "$make3" = "make: Nothing to be done for \`all'." ] ; then
-		if [ "$make2" = "make: Nothing to be done for \`all'." ] ; then
-			echo "Makefile : \033[32;1mOK\033[00;0m"
+		shift
+	done
+	if [[ autor -eq 1 || all -eq 1 ]]; then
+		if [ -e "auteur" ]; then
+			user=$(cat auteur)
+			iam=$(whoami)
+			if [ "$user" = "$iam" ] ; then
+				echo "Author file : \033[32;1mOK\033[00;0m"
+			else
+				echo "Author file : \033[31;1mKO\033[00;0m"
+			fi
 		else
-			echo "Makefile : \033[31;1mKO\033[00;0m"
+			echo "\033[31;1mAuthor file missing\033[00;0m"
 		fi
-	else
-		echo "\033[31;1mMakefile missing\033[00;0m"
+		echo
 	fi
-	echo
-fi
-if [[ norme -eq 1 || all -eq 1 ]]; then
-	norme_error=$(norminette . | grep -B1 Error | grep -w "Error" -c)
-	if [ $norme_error != 0 ] ; then
-		echo "Norminette : \033[31;1mKO\033[00;0m"
-		norminette . | grep -B1 Error
-	else
-		echo "Norminette : \033[32;1mOK\033[00;0m"
+	if [[ makefile -eq 1 || all -eq 1 ]]; then
+		if [ -e "Makefile" ]; then
+			make fclean
+			make
+			make2=$(make)
+			make clean
+			# make3=$(make)
+			# make re
+			make
+			make fclean
+			# if [ "$make2" = "make: Nothing to be done for \`all'." -a "$make3" = "make: Nothing to be done for \`all'." ] ; then
+			if [ "$make2" = "make: Nothing to be done for \`all'." ] ; then
+				echo "Makefile : \033[32;1mOK\033[00;0m"
+			else
+				echo "Makefile : \033[31;1mKO\033[00;0m"
+			fi
+		else
+			echo "\033[31;1mMakefile missing\033[00;0m"
+		fi
+		echo
 	fi
-	echo
-fi
-if [ -e "lemintest.log" ]; then
-	rm -rf lemintest.log
-else
-	touch lemintest.log
-fi
-make fclean && make
+	if [[ norme -eq 1 || all -eq 1 ]]; then
+		norme_error=$(norminette . | grep -B1 Error | grep -w "Error" -c)
+		if [ $norme_error != 0 ] ; then
+			echo "Norminette : \033[31;1mKO\033[00;0m"
+			norminette . | grep -B1 Error
+		else
+			echo "Norminette : \033[32;1mOK\033[00;0m"
+		fi
+		echo
+	fi
+	if [ -e "lemintest.log" ]; then
+		rm -rf lemintest.log
+	else
+		touch lemintest.log
+	fi
+	make fclean && make
 # ---------------------------------------------------------------------------- #
+
+# printf "%-35s\n" "$yellow$(basename $f)$normal" >> lemintest.log
+# echo $err  >> lemintest.log
 
 # Errors tests
 # ---------------------------------------------------------------------------- #
-echo "$blue~ERRORS~$normal"
-for f in lem-in_maps/error/*/*
+# echo "$blue~ERRORS~$normal"
+for d in lem-in_maps/error/*
 do
-	err=$($leaks ./lem-in < $f)
-	lik=$?
-	printf "%-35s\n" "$yellow$(basename $f)$normal" >> lemintest.log
-	echo $err  >> lemintest.log
-	printf "%-35s" "$yellow$(basename $f)$normal"
-	signal $lik
-	if [[ leaks == "" ]]; then
-		if [ $signres -eq 0 ]; then
-			if [[ $err == *"error"* || $err == *"ERROR"* || $err == *"Error"* ]]; then
-				printf "%30s\n" "$green[OK]$normal"
-			else
-				printf "%30s\n" "$red[KO]$normal"
-			fi
-		fi
-	else
-		if [ $signres -eq 0 ]; then
-			if [[ $err == *"error"* || $err == *"ERROR"* || $err == *"Error"* ]]; then
-				printf "%30s\n" "$green[OK]$normal"
-			elif [[ $lik -eq 42 ]]; then
-				printf "%33s\n" "$red[LEAKS]$normal"
-			else
-				printf "%30s\n" "$red[KO]$normal"
-			fi
-		fi
-	fi
+	printf "%-40s" "$yellow$(basename $d) : $normal"
+	for f in lem-in_maps/error/$(basename $d)/*
+	do
+		err=$($leaks ./lem-in < $f)
+		lik=$?
+		signalerr $lik "$err" $i $f
+	done
+	printf "\n"
+
 done
-# ---------------------------------------------------------------------------- #
 
-
-# Tests with comments
-# ---------------------------------------------------------------------------- #
-echo "\n$blue~COMMENTS~$normal"
+printf "%-40s" "$yellow""Comments : ""$normal"
 for f in lem-in_maps/comment/*
 do
-	leak=$($leaks ./lem-in < $f >> lemintest.log)
+	leak=$($leaks ./lem-in < $f)
 	lik=$?
 	comm=$(bash -c 'diff -u <(cat '$f') <(./lem-in < '$f')')
-	printf "%-35s\n" "$yellow$(basename $f)$normal" >> lemintest.log
-	echo $leak  >> lemintest.log
-	printf "%-35s" "$yellow$(basename $f)$normal"
-	signal $lik
-	if [[ leaks == "" ]]; then
-		if [ $signres -eq 0 ]; then
-			if [ "$comm" ]; then
-				printf "%30s\n" "$red[KO]$normal"
-			else
-				printf "%30s\n" "$green[OK]$normal"
-			fi
-		fi
-	else
-		if [ $signres -eq 0 ]; then
-			if [[ "$comm" ]]; then
-				printf "%30s\n" "$red[KO]$normal"
-			elif [[ $lik -eq 42 ]]; then
-				printf "%33s\n" "$red[LEAKS]$normal"
-			else
-				printf "%30s\n" "$green[OK]$normal"
-			fi
-		fi
-	fi
+	signal $lik "$comm" $i $f
 done
-# ---------------------------------------------------------------------------- #
+printf "\n"
 
-# Tests with cmd
-# ---------------------------------------------------------------------------- #
-echo "\n$blue~CMD~$normal"
+
+printf "%-40s" "$yellow""Cmd : ""$normal"
 for f in lem-in_maps/cmd/*
 do
 	leak=$($leaks ./lem-in < $f)
@@ -205,67 +178,33 @@ do
 	else
 		comm=$(bash -c 'diff -u <(cat '$WAY'/cmd_trace/cmd_trace_alpha) <(./lem-in < '$f')')
 	fi
-	printf "%-35s\n" "$yellow$(basename $f)$normal" >> lemintest.log
-	echo "$comm\n"  >> lemintest.log
-	printf "%-35s" "$yellow$(basename $f)$normal"
-	signal $lik
-	if [[ leaks == "" ]]; then
-		if [ $signres -eq 0 ]; then
-			if [ "$comm" ]; then
-				printf "%30s\n" "$red[KO]$normal"
-			else
-				printf "%30s\n" "$green[OK]$normal"
-			fi
-		fi
-	else
-		if [ $signres -eq 0 ]; then
-			if [[ "$comm" ]]; then
-				printf "%30s\n" "$red[KO]$normal"
-			elif [[ $lik -eq 42 ]]; then
-				printf "%33s\n" "$red[LEAKS]$normal"
-			else
-				printf "%30s\n" "$green[OK]$normal"
-			fi
-		fi
-	fi
+	signal $lik "$comm" $i $f
 done
-# ---------------------------------------------------------------------------- #
+printf "\n"
 
-# Tests with cmd
-# ---------------------------------------------------------------------------- #
-echo "\n$blue~PIPES ERROR~$normal"
+
+printf "%-40s" "$yellow""pipes_error : ""$normal"
 for f in lem-in_maps/pipes_error/*
 do
 	leak=$($leaks ./lem-in < $f)
 	lik=$?
 	comm=$(bash -c 'diff -u <(cat '$WAY'/pipes_error_trace/'$(basename $f)') <(./lem-in < '$f')')
-	printf "%-35s\n" "$yellow$(basename $f)$normal" >> lemintest.log
-	echo "$comm\n"  >> lemintest.log
-	printf "%-35s" "$yellow$(basename $f)$normal"
-	# $leaks ./lem-in < $f
-	signal $lik
-	if [[ leaks == "" ]]; then
-		if [ $signres -eq 0 ]; then
-			if [ "$comm" ]; then
-				printf "%30s\n" "$red[KO]$normal"
-			else
-				printf "%30s\n" "$green[OK]$normal"
-			fi
-		fi
-	else
-		if [ $signres -eq 0 ]; then
-			if [[ "$comm" ]]; then
-				printf "%30s\n" "$red[KO]$normal"
-			elif [[ $lik -eq 42 ]]; then
-				printf "%33s\n" "$red[LEAKS]$normal"
-			else
-				printf "%30s\n" "$green[OK]$normal"
-			fi
-		fi
-	fi
+	signal $lik "$comm" $i $f
 done
-# ---------------------------------------------------------------------------- #
+printf "\n"
 
+
+if [ ${#errors[@]} -ne 0 ]; then
+echo "-------------------------------------------------------------------------"
+echo "$red""Errors in""$normal"
+	for nb in "${errors[@]}"
+	do
+		echo "$nb : \c"
+		echo $red${motif[$j]}$normal
+		j=$((j+1))
+	done
+fi
+printf "\n"
 
 
 
