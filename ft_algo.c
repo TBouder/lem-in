2d487f6de099f8442bf80de8f08a839694d33218
tbouder <tbouder@student.42.fr>
2016-05-03 16:10:58 +0200

    Auto-Update 16:10:58, Tuesday 03 May

--- ft_algo.c
+++ ft_algo.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/02 12:07:02 by tbouder           #+#    #+#             */
-/*   Updated: 2016/05/03 12:57:35 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/05/03 16:01:00 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,43 +32,18 @@ char	*ft_push_path(char **s1, char *s2)
 	return (str);
 }
 
-void	ft_find_path_2(t_env *env, t_path *path, t_rooms *r, int i)
+int	ft_finish_path(t_env *env, t_path *path)
 {
-	t_pipes *tmp;
 	char	**str;
-	int		nb;
 
-	while (1)
-	{
-		nb = 1;
-		str = ft_strsplit(path->path, ' ');
-		if (!ft_strcmp(str[ft_dbstrlen(str) - 1], ft_find_end(env->rooms)->name))
-			break ;
-		else
-		{
-			tmp = r->pipes_next;
-			while (i-- > 0)
-				tmp = tmp->next;
-			if (!tmp || ft_strstr(path->path, ft_find_room(env->rooms, tmp)->name))
-				break ;
-			else if (ft_pipeslen(tmp) == 1)
-			{
-				// if (ft_strstr(path->path, ft_find_room(env->rooms, tmp)->name))
-					// break ;
-				path->path = ft_push_path(&path->path, ft_find_room(env->rooms, tmp)->name);
-				r = ft_find_room(env->rooms, tmp);
-			}
-			else
-			{
-				while (nb++ < ft_pipeslen(tmp))
-					ft_pathsend(&path, path->path);
-				r = ft_find_room(env->rooms, tmp);
-				path->path = ft_push_path(&path->path, ft_find_room(env->rooms, tmp)->name);
-			}
-
-		}
-		ft_dbstrdel(str);
-	}
+	str = ft_strsplit(path->path, ' ');
+	if (!ft_strcmp(str[ft_dbstrlen(str) - 1], "ERROR"))
+		return (0);
+	else if (ft_strcmp(str[ft_dbstrlen(str) - 1], ft_find_end(env->rooms)->name))
+		return (1);
+	else
+		return (2);
+	ft_dbstrdel(str);
 }
 
 void	ft_algo(t_env *env)
@@ -76,22 +51,23 @@ void	ft_algo(t_env *env)
 	ft_putendl("\n");
 
 	t_path	*path;
-	t_path	*tmp;
-	// char	*best;
+	int		nb;
 
 	path = NULL;
 	ft_pathsend(&path, ft_find_start(env->rooms)->name);
-	tmp = path;
-	// int	j = 0;
-	// while (tmp)
-	// {
-		// j = ft_pipeslen(ft_find_room_s(env->rooms, tmp->path)->pipes_next) - 1;
-		// ft_find_path_2(env, tmp, ft_find_room_s(env->rooms, tmp->path), j);
-		ft_find_path(env, path);
-		// ft_find_path(env, tmp, ft_find_room(env->rooms, );
-		// tmp = tmp->next;
-	// }
-
-
-	ft_print_path(path);
+	ft_find_path(env, path);
+	ft_putendl(path->path);
+	while (path)
+	{
+		ft_putendl(path->path);
+		nb = ft_finish_path(env, path);
+		if (nb == 1)
+			ft_printf("{y}A Completer{0}\n");
+		if (nb == 2)
+			ft_printf("{g}FINIIIIIIIIIIIT{0}\n");
+		if (nb == 0)
+			ft_printf("{r}Terminate him{0}\n");
+		path = path->next;
+	}
+	// ft_print_path(path);
 }
