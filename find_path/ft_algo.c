c5596a0c7806eed31e52404744421a9230154e66
Tbouder <tbouder@protonmail.com>
2016-05-11 14:51:13 +0200

    Auto-Update 14:51:13, Wednesday 11 May

--- find_path/ft_algo.c
+++ find_path/ft_algo.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/02 12:07:02 by tbouder           #+#    #+#             */
-/*   Updated: 2016/05/11 14:18:35 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/05/11 14:50:43 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,36 @@ static int	ft_found_less_path(t_path *path, t_env *env)
 	return (i);
 }
 
+ void	ft_verif_collision(t_path *path, t_path	*tmp)
+{
+	int		i;
+	char	**s1;
+	char	**s2;
+
+	while (path)
+	{
+		ft_printf("{r}%s{0}\n", path->path);
+		tmp = path->next;
+		s1 = ft_strsplit(path->path, ' ');
+		while (tmp)
+		{
+			i = 0;
+			s2 = ft_strsplit(tmp->path, ' ');
+			while (s1[i] && s2[i])
+			{
+				// while (ft_isstrstr(s1[i], s2[i]))
+				if (ft_isstrstr(s1[i], s2[i]))
+					tmp->path = ft_push_path(&tmp->path, "ERROR");
+				i++;
+			}
+			tmp = tmp->next;
+			ft_dbstrdel(s2);
+		}
+		ft_dbstrdel(s1);
+		path = path->next;
+	}
+}
+
 void		ft_algo(t_env *env)
 {
 	t_path	*path;
@@ -81,11 +111,19 @@ void		ft_algo(t_env *env)
 			break ;
 		path = path->next;
 	}
+
 	ft_verif_same_path(origin);
+	ft_verif_collision(origin, NULL);
 	ft_path_remove_if_error(&origin, "ERROR");
-	// ft_putendl("----------");
-	// ft_print_path(origin);
-	// ft_putendl("----------\n");
+	ft_putendl("----------");
+	ft_print_path(origin);
+	ft_putendl("----------\n");
 	env->path = ft_strinit(origin->path);
+	path = origin;
+	while (path)
+	{
+		ft_strdel(&path->path);
+		path = path->next;
+	}
 	ft_path_clear(&origin);
 }
