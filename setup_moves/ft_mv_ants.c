1ed93c250dcf49598db593fd01ae8764a2af6070
Tbouder-mac <tbouder@student.42.fr>
2016-05-12 19:09:40 +0200

    Auto-Update 19:09:40, Thursday 12 May

--- setup_moves/ft_mv_ants.c
+++ setup_moves/ft_mv_ants.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/10 11:47:18 by tbouder           #+#    #+#             */
-/*   Updated: 2016/05/12 18:38:55 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/05/12 19:03:50 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,18 +48,67 @@ t_rooms	*ft_find_alast_ant(t_rooms *rooms, char **str)
 void	ft_mv_ants(t_env *env, char **str)
 {
 	int		i;
+	int		j;
 	int		id;
 
 	i = 0;
+	j = 0;
 	id = 1;
-	while (str[i])
+	if (str[i + 1] && CURRENT->ant >= 1 && NEXT->ant == 0)
 	{
-		if (str[i + 1] && CURRENT->ant >= 1 && NEXT->ant == 0)
-		{
-			ft_display_move(id, NEXT->name);
-			CURRENT->ant -= 1;
-			NEXT->ant += 1;
-		}
-		i++;
+		ft_display_move(id, NEXT->name);
+		CURRENT->ant -= 1;
+		NEXT->ant += 1;
+	}
+
+ft_putchar('\n');
+
+	j++;
+	i = j;
+	if (str[i + 1] && CURRENT->ant >= 1 && NEXT->ant == 0)
+	{
+		ft_display_move(id, NEXT->name);
+		CURRENT->ant -= 1;
+		NEXT->ant += 1;
+	}
+	i = 0;
+	if (str[i + 1] && CURRENT->ant >= 1 && NEXT->ant == 0)
+	{
+		ft_display_move(id + j, NEXT->name);
+		CURRENT->ant -= 1;
+		NEXT->ant += 1;
+	}
+
+ft_putchar('\n');
+
+	j++;
+	i = j;
+	if (str[i + 1] && CURRENT->ant >= 1 && NEXT->ant == 0)
+	{
+		ft_display_move(id, NEXT->name);
+		CURRENT->ant -= 1;
+		NEXT->ant += 1;
+	}
+	i = j - 1;
+	if (str[i + 1] && CURRENT->ant >= 1 && NEXT->ant == 0)
+	{
+		ft_display_move(id + j - 1, NEXT->name);
+		CURRENT->ant -= 1;
+		NEXT->ant += 1;
+	}
+	i = 0;
+	if (str[i + 1] && CURRENT->ant >= 1 && NEXT->ant == 0)
+	{
+		ft_display_move(id + j, NEXT->name);
+		CURRENT->ant -= 1;
+		NEXT->ant += 1;
 	}
 }
+
+// [0]	[2]	[3]	[1]
+//  3	 0	 0	 0
+//  2	 1	 0	 0
+//  1	 1	 1	 0
+//  0	 1	 1	 1
+//  0	 0	 1	 2
+//  0	 0	 0	 3
