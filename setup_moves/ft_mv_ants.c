d498cd61259ba410d30d580e503bb47a4c435523
Tbouder <tbouder@protonmail.com>
2016-05-12 18:04:08 +0200

    Auto-Update 18:04:08, Thursday 12 May

--- setup_moves/ft_mv_ants.c
+++ setup_moves/ft_mv_ants.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/10 11:47:18 by tbouder           #+#    #+#             */
-/*   Updated: 2016/05/12 13:27:03 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/05/12 18:03:07 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,46 +49,36 @@ void	ft_mv_ants(t_env *env, char **str)
 {
 	int		i;
 	int		j;
-	int		verif;
-
-	i = 0;
-	verif = 0;
-	while (str[i])
-	{
-		ft_putstr(CURRENT->name); ft_putchar(' ');
-		ft_putnbr(CURRENT->ant); ft_putchar('\n');
-		i++;
-	}
-
-ft_putchar('\n');
 
 	i = 0;
 	j = 0;
-	verif = 0;
-	while (str[i])
+
+	if (str[i] != NULL && CURRENT->ant >= 1)
 	{
-		if (str[i] != NULL && CURRENT->ant >= 1)
+		if (str[i + 1] && NEXT->ant == 0)
 		{
-			if (str[i + 1] && NEXT->ant == 0)
-			{
-				NEXT->ant += 1;
-				CURRENT->ant -= 1;
-				// i = 0;
-			}
-			ft_putstr(CURRENT->name); ft_putchar(' ');
-			ft_putnbr(CURRENT->ant); ft_putchar('\n');
+			NEXT->ant += 1;
+			CURRENT->ant -= 1;
+			// i = 0;
 		}
-		// if (str[i + 1] && NEXT->ant == 0 && CURRENT->ant >= 1 && verif == 0)
-		// {
-		// 	NEXT->ant += 1;
-		// 	CURRENT->ant -= 1;
-		// 	verif++;
-		// }
-		// ft_putstr(CURRENT->name); ft_putchar(' ');
-		// ft_putnbr(CURRENT->ant); ft_putchar(' ');
-		// ft_nbrendl(CURRENT->ant_id);
-		i++;
 	}
-	if (0)
-		env->ant = 0;
+ft_putstr(CURRENT->name); ft_putchar(' ');
+ft_putnbr(CURRENT->ant); ft_putchar('\n');
+	i++;
+ft_putstr(CURRENT->name); ft_putchar(' ');
+ft_putnbr(CURRENT->ant); ft_putchar('\n');
+	if (str[i] != NULL && CURRENT->ant >= 1)
+	{
+		if (str[i + 1] && NEXT->ant == 0)
+		{
+			NEXT->ant += 1;
+			CURRENT->ant -= 1;
+			// i = 0;
+		}
+	}
+ft_putstr(CURRENT->name); ft_putchar(' ');
+ft_putnbr(CURRENT->ant); ft_putchar('\n');
+	i++;
+ft_putstr(CURRENT->name); ft_putchar(' ');
+ft_putnbr(CURRENT->ant); ft_putchar('\n');
 }
