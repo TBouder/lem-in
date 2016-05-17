eded08e02f406227c4b2e65fd05324fdf6edc63c
Tbouder <tbouder@protonmail.com>
2016-05-17 18:58:09 +0200

    Auto-Update 18:58:09, Mardi 17 mai

--- setup_moves/ft_moves.h
+++ setup_moves/ft_moves.h
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/10 22:10:56 by tbouder           #+#    #+#             */
-/*   Updated: 2016/05/16 15:52:17 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/05/17 18:36:49 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "../ft_lemin.h"
 
-int		*ft_init_tab(t_env *env);
+int		*ft_init_tab(int ant);
 void	ft_print_moves(t_env *env, int *nbr, char **str);
 
 void	ft_display_move(int ant_id, char *room_name);
