4ed50d7c9f1a47af5cf66859d9d7d18ca9d403d3
Tbouder <tbouder@protonmail.com>
2016-05-17 12:41:52 +0200

    Auto-Update 12:41:52, Tuesday 17 May

--- setup_moves/ft_moves.c
+++ setup_moves/ft_moves.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/10 22:12:20 by tbouder           #+#    #+#             */
-/*   Updated: 2016/05/16 15:52:02 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/05/17 12:36:20 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,18 @@ void	ft_moves(t_env *env)
 	char	**str;
 	int		*nbr;
 
-	nbr = ft_init_tab(env);
-	str = ft_strsplit(env->paths->path, ' ');
-	ft_print_moves(env, nbr, str);
-	ft_dbstrdel(str);
-	free(nbr);
+	if (env->paths && env->paths->next == NULL)
+	{
+		str = ft_strsplit(env->paths->path, ' ');
+		if (ft_strequ(str[1], env->end->name))
+			nbr = ft_nbrnew(env->ant);
+		else
+			nbr = ft_init_tab(env);
+		ft_print_moves(env, nbr, str);
+		ft_dbstrdel(str);
+		free(nbr);
+	}
 	ft_clear_gnl(env);
 	ft_free_env(&env, 1);
+
 }
