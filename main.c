e55f3d00f188701fffc542ad931158d53d9fc82f
Tbouder <tbouder@protonmail.com>
2016-05-30 16:42:39 +0200

    End lem-in + bonus

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/10 18:22:57 by tbouder           #+#    #+#             */
-/*   Updated: 2016/05/30 16:23:41 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/05/30 16:42:26 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 /*
 ** BONUS
-** - Lecture plusieurs fichiers a la fois (Neccessite OPEN)
+** - Can read more multiple files one after the other (with open)
 ** - Explicits errors
 ** - Possibility to use -s for soft mode (neither same pipes, nor pipe-to-itself
 **		room are an error)
 ** - Possibility to use -p for path mode (Display the differents paths used)
 ** - Possibility to use -c for color mode
 ** - Possibility to use -d for distance mode
+** - Possibility to use -i for info mode
 */
 
 int			main(int ac, char **av)
---
+++ setup_moves/ft_shortest_helper.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_shortest_helper.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/30 14:46:11 by tbouder           #+#    #+#             */
+/*   Updated: 2016/05/30 14:56:21 by tbouder          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_moves.h"
+
+int		***ft_init_trplnbr(t_path *origin)
+{
+	int		j;
+	int		k;
+	int		len;
+	int		***nbr;
+	t_path	*path;
+
+	path = origin;
+	!(nbr = (int ***)malloc(sizeof(int **) * ft_path_len(path))) ? exit(1) : 0;
+	j = 0;
+	while (path)
+	{
+		len = ft_strcountchar(path->path, ' ');
+		!(nbr[j] = (int **)malloc(sizeof(int *) * len)) ? exit(1) : 0;
+		k = 0;
+		while (k < len)
+		{
+			nbr[j][k] = ft_nbrnew(2);
+			k++;
+		}
+		j++;
+		path = path->next;
+	}
+	return (nbr);
+}
+
+void	ft_free_trplnbr(t_path *origin, int ***nbr)
+{
+	int		j;
+	int		k;
+	int		len;
+	t_path	*path;
+
+	path = origin;
+	j = 0;
+	while (path)
+	{
+		len = ft_strcountchar(path->path, ' ');
+		k = 0;
+		while (k < len - 1)
+			free(nbr[j][k++]);
+		free(nbr[j]);
+		j++;
+		path = path->next;
+	}
+	free(nbr);
+}
